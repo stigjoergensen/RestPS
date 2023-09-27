604c50ceaaeff00ed66ddd3f7e7d37f2033395e4
Stig Joergensen <stig.joergensen@clearsky.dk>
2023-09-27 11:14:47 +0200

    Update Get-RestIPAuth.ps1

--- RestPS/bin/Get-RestIPAuth.ps1
+++ RestPS/bin/Get-RestIPAuth.ps1
@@ -3,18 +3,23 @@ function Get-RestIPAuth
     $RestIPAuth = @{
         ACL                = @{
             "127.0.0.1"                  = @{
+                Name = "Local host"
                 Path = @("/")                   # Local host have access to everything
             }
             "192.168.22.18"              = @{
+                Name = "Workstation 1"
                 Path = @("/endpoint/shutdown", "/endpoint/status")
             }
             "192.168.22.5"               = @{
+                Name = "Workstatation 2"
                 Path = @("/a", "/b")
             }
             "192.168.22.5,192.168.22.18" = @{	# Define a comma list of IPs with access, needs to be enabled, see below
+                Name = "Workstation 1 and 2"
                 Path = @("/c", "/d")
             }
             "192.168.1.0/24"             = @{   # you are able to define a subnet with access, but subnetsearch must be enabled before it will work, see below
+                Name "Subnet 1"
                 Path = @("/")
             }
         }
